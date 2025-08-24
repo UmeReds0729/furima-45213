858dfb881a14e14848821d0f7e3b651ca374991d
UmeReds0729 <ume.reds-isotope@nifty.com>
2025-08-24 19:15:24 +0900

    #4ユーザー管理機能：新規ユーザー登録・ログインページの実装

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validate :password_complexity
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :birthday, presence: true
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    return if password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/)
+
+    errors.add :password, 'は半角英数字を両方含めて設定してください'
+  end
+end
