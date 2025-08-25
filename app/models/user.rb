21e4e288ff38cb9160bbb52a1a210cdf165cd0d6
UmeReds0729 <ume.reds-isotope@nifty.com>
2025-08-25 23:48:50 +0900

    #4ユーザー管理機能：モデルの単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validate :password_complexity
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters', allow_blank: true }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters', allow_blank: true }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'must be full-width katakana', allow_blank: true }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'must be full-width katakana', allow_blank: true }
   validates :birthday, presence: true
 
   private
@@ -17,6 +17,6 @@ class User < ApplicationRecord
 
     return if password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/)
 
-    errors.add :password, 'は半角英数字を両方含めて設定してください'
+    errors.add :password, 'must include both letters and numbers'
   end
 end
