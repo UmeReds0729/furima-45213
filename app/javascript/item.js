1d2b083501bfee1b089f27ae051d9879bfd5501c
UmeReds0729 <ume.reds-isotope@nifty.com>
2025-08-28 23:23:57 +0900

    #5商品出品機能：商品出品ページのJS微修正

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,13 +1,15 @@
-let itemPrice = document.getElementById("item-price")
+document.addEventListener("DOMContentLoaded", () => {
+  let itemPrice = document.getElementById("item-price")
 
-itemPrice.addEventListener("input", () => {
-  let price = Number(itemPrice.value)
-  let commission = price * 0.1
-  let profit = price - commission
+  itemPrice.addEventListener("input", () => {
+    let price = Number(itemPrice.value)
+    let commission = price * 0.1
+    let profit = price - commission
 
-  let commissionArea = document.getElementById("add-tax-price");
-  let profitArea = document.getElementById("profit");
+    let commissionArea = document.getElementById("add-tax-price");
+    let profitArea = document.getElementById("profit");
 
-  commissionArea.textContent = commission;
-  profitArea.textContent = profit;
+    commissionArea.textContent = commission;
+    profitArea.textContent = profit;
+  });
 });
